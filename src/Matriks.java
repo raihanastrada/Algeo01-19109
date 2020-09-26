6e3dd83eb5a85b83a15254572d842967bf249a70
abiwardani <abi.wardani85@gmail.com>
2020-09-26 20:01:00 +0700

    tambah method matrix multiplication dan determinan pake ekspansi kofaktor

--- src/Matriks.java
+++ src/Matriks.java
@@ -40,4 +40,43 @@ public class Matriks {
         }
     }
 
+    public Matriks KaliMatriks(Matriks M2) {
+        Matriks M1 = new Matriks(M2.NBrsEff, this.NKolEFF);
+        for (int i = 0; i < this.NBrsEff; i++) {
+            for (int j = 0; j < this.NKolEFF; j++) {
+                M1.Mat[i][j] = 0;
+                for (int k = 0; k < this.NBrsEff; k++) {
+                    M1.Mat[i][j] += Mat[i][k]*M2.Mat[k][j];
+                }
+            }
+        }
+        return M1;
+    }
+
+    public float Determinan() {
+        float d = 0;
+        if ((this.NBrsEff == 1) && (this.NKolEFF == 1)) {
+            return Mat[0][0];
+        } else {
+            int j, i1, j1, j2;
+            int s = 1;
+            Matriks Cofact;
+            for (j = 0; j < this.NKolEFF; j++) {
+                Cofact = new Matriks(this.NBrsEff-1, this.NKolEFF-1);
+                for (i1 = 1; i1 < this.NBrsEff; i1++) {
+                    j2 = 0;
+                    for (j1 = 0; j1 < this.NKolEFF; j1++) {
+                        if (j1 != j) {
+                            Cofact.Mat[i1-1][j2] = Mat[i1][j1];
+                            j2++;
+                        }
+                    }
+                }
+
+                d += Mat[0][j] * s * Cofact.Determinan();
+                s *= -1;
+            }
+            return d;
+        }
+    }
 }
