d7abbbfe12ddb4f13228c2412fb515d92866c6b0
abiwardani <abi.wardani85@gmail.com>
2020-10-01 19:25:08 +0700

    transisi ke print/save based output

--- src/Matriks.java
+++ src/Matriks.java
@@ -57,11 +57,11 @@ public class Matriks {
                     }
                 }
             } catch (Exception e) {
-                System.out.println("File not found");
+                System.out.println("File tidak ditemukan.");
                 e.printStackTrace();
             }
         } catch (Exception e) {
-            System.out.println("File not found");
+            System.out.println("File tidak ditemukan.");
             e.printStackTrace();
         }
     }
@@ -81,17 +81,21 @@ public class Matriks {
         return b;
     }
 
-    public void TulisMatriks() {
+    public String[] TulisMatriks() {
+        String[] output = new String[this.NBrsEff];
+        String outputln;
         for (int i = 0; i < this.NBrsEff; i++) {
+            outputln = "";
             for (int j = 0; j < this.NKolEFF; j++) {
                 if (j != this.NKolEFF - 1) {
-                    System.out.print(Mat[i][j] + " ");
+                    outputln += Mat[i][j] + " ";
                 } else {
-                    System.out.println(Mat[i][j]);
+                    outputln += Mat[i][j];
                 }
             }
+            output[i] = outputln;
         }
-        System.out.println("");
+        return output;
     }
 
     public Matriks KaliMatriks(Matriks M2) {
@@ -222,7 +226,7 @@ public class Matriks {
         return Inv;
     }
 
-    public void Crammer(Matriks MHsl) {
+    public String[] Crammer(Matriks MHsl) {
         double D = this.Determinan2();
         double[] arrHsl = new double[this.NKolEFF];
         Matriks tempMat = new Matriks(this.NBrsEff,this.NKolEFF);
@@ -231,20 +235,19 @@ public class Matriks {
                 tempMat.Mat[i][j] = Mat[i][j];
                 Mat[i][j] = MHsl.Mat[i][0];
             }
-            TulisMatriks();
+            // TulisMatriks();
             arrHsl[j] = (this.Determinan()) / D; // Menghitung D/D[n]
             for (int k = 0; k < this.NBrsEff; k++) {
                 Mat[k][j] = tempMat.Mat[k][j]; // Mengembalikan kolom
             }
         }
+        String[] output = new String[this.NKolEFF];
+        String outputln;
         for (int i = 0; i < this.NKolEFF; i++) {
-            System.out.print("solusi dari X" + (i + 1) + " adalah = ");
-            System.out.print(arrHsl[i]);
-            if (i != this.NKolEFF - 1) {
-                System.out.print(", ");
-            }
+            outputln = "Solusi dari X" + (i + 1) + " adalah = " + arrHsl[i];
+            output[i] = outputln;
         }
-        System.out.println("");
+        return output;
     }
 
     public Matriks Gauss(Matriks Mkonstan) {
@@ -431,54 +434,57 @@ public class Matriks {
         return MAugmented;
     }
 
-    public void TulisSPLUnik() {
+    public String[] TulisSPLUnik() {
+        String[] output = new String[this.NBrsEff];
+        String outputln = "";
         for (int i = 0; i < this.NBrsEff; i++) {
-            System.out.print("Solusi dari X" + (i + 1) + " adalah = ");
-            System.out.println(Mat[i][NKolEFF-1]);
+            outputln = "Solusi dari X" + (i + 1) + " adalah = " + Mat[i][this.NKolEFF-1];
+            output[i] = outputln;
         }
+        return output;
     }
 
-    public void TulisSPLParametrik(Matriks M1, Matriks b1) {
-        double epsilon = 1e-10;
-        Matriks A = this.Copy();
-        Matriks bvar = b1.VarMask(M1);
-        Matriks Mkonst = A.KaliMatriks(b1.UnMask(bvar));
+    public String[] TulisSPLInvParametrik(Matriks M1, Matriks b1, int nvar) { // memroses SPL yang butuh solusi parametrik
+        double epsilon = 1e-10; // define epsilon untuk toleransi angka kecil
+        Matriks A = this.Copy(); // biar tidak ganggu matriks this
+        Matriks bvar = b1.VarMask(M1); // mencari tahu kolom mana saja berbentuk variabel/parameter
+        Matriks Mkonst = A.KaliMatriks(b1.UnMask(bvar)); // x = A^-1 * b1 untuk mencari konstanta yang berada di masing solusi
+        String[] output = new String[nvar];
         String outputln = "";
-        for (int i = 0; i < A.NBrsEff; i++) {
+        for (int i = 0; i < nvar; i++) { 
             outputln = "";
-            if ((Mkonst.Mat[i][0] > epsilon )|| (Mkonst.Mat[i][0] < -epsilon)) {
-                outputln += (Mkonst.Mat[i][0]);
+            if ((Mkonst.Mat[i][0] > epsilon )|| (Mkonst.Mat[i][0] < -epsilon)) { // kalau konstanta bukan 0
+                outputln += (Mkonst.Mat[i][0]); // cetak konstanta
             }
             for (int j = 0; j < A.NKolEFF; j++) {
-                if (bvar.Mat[j][0] != 0) {
-                    if (A.Mat[i][j] > epsilon) {
-                        if ((Mkonst.Mat[i][0] > epsilon )|| (Mkonst.Mat[i][0] < -epsilon)) {
-                            outputln += " + ";
+                if (bvar.Mat[j][0] != 0) { // kalau mask variabel bukan 0
+                    if (A.Mat[i][j] > epsilon) { // kalau koefisien variabel positif
+                        if ((Mkonst.Mat[i][0] > epsilon )|| (Mkonst.Mat[i][0] < -epsilon)) { // kalau tadi mencetak konstanta
+                            outputln += " + "; // cetak tanda "+ <variabel>"
                         }
-                        if (A.Mat[i][j] != 1) {
+                        if (A.Mat[i][j] != 1) { // kalau koefisien bukan 1, cetak koefnya
                             outputln += A.Mat[i][j];
                         }
-                        outputln += "k" + ((int) bvar.Mat[j][0]);
-                    } else if (A.Mat[i][j] < -epsilon) {
-                        outputln += " - ";
-                        if (A.Mat[i][j] != -1) {
+                        outputln += "k" + ((int) bvar.Mat[j][0]); // cetak variabel k keberapa
+                    } else if (A.Mat[i][j] < -epsilon) { // kalau koefisien variabel negatif
+                        outputln += " - "; // cetak tanda "- <variabel>"
+                        if (A.Mat[i][j] != -1) { // kalau koefisien bukan 1, cetak koefnya
                             outputln += (-1*A.Mat[i][j]);
                         }
-                        outputln += "k" + ((int) bvar.Mat[j][0]);
+                        outputln += "k" + ((int) bvar.Mat[j][0]); // cetak variabel k keberapa
                     }
                 }
             }
             if (outputln == "") {
-                outputln = "0.0";
+                outputln = "0.0"; // kalau masih kosong berarti hanya konstanta 0 saja
             }
-            if (!(M1.AmbilKolom(i).IsEmpty())) {
-                outputln = "Solusi dari X" + (i + 1) + " adalah = " + outputln;
-                System.out.println(outputln);
-            }
+            outputln = "Solusi dari X" + (i + 1) + " adalah = " + outputln; // rangkai string output line ke-i
+            output[i] = outputln;
         }
+        return output;
     }
 
-    public Matriks TukerKolom(Matriks k, int j) {
+    public Matriks TukerKolom(Matriks k, int j) { // menukar kolom ke-j matriks dengan matriks kolom k
         Matriks M1 = this.Copy();
         for (int i = 0; i < this.NBrsEff; i++) {
             M1.Mat[i][j] = k.Mat[i][0];
@@ -486,7 +492,7 @@ public class Matriks {
         return M1;
     }
 
-    public Matriks TukerBaris(Matriks b, int i) {
+    public Matriks TukerBaris(Matriks b, int i) { // menukar baris ke-i matriks dengan matriks baris b
         Matriks M1 = this.Copy();
         for (int j = 0; j < this.NKolEFF; j++) {
             M1.Mat[i][j] = b.Mat[0][j];
@@ -494,7 +500,7 @@ public class Matriks {
         return M1;
     }
 
-    public boolean AdaBasis(int Loc) {
+    public boolean AdaBasis(int Loc) { // periksa apakah ada baris basis dengan elemen 1 di kolom ke-Loc
         boolean ada = false;
         boolean adaBrs;
         for (int i = 0; i < this.NBrsEff; i++) {
@@ -507,7 +513,7 @@ public class Matriks {
         return ada;
     }
 
-    public Matriks AmbilBaris(int i) {
+    public Matriks AmbilBaris(int i) { // salin baris ke-i dalam matriks
         Matriks Hasil = new Matriks(1, this.NKolEFF);
         for (int j = 0; j < this.NKolEFF; j++) {
             Hasil.Mat[0][j] = Mat[i][j];
@@ -515,7 +521,7 @@ public class Matriks {
         return Hasil;
     }
 
-    public Matriks AmbilKolom(int j) {
+    public Matriks AmbilKolom(int j) { // salin kolom ke-j dalam matriks
         Matriks Hasil = new Matriks(this.NBrsEff, 1);
         for (int i = 0; i < this.NBrsEff; i++) {
             Hasil.Mat[i][0] = Mat[i][j];
@@ -523,7 +529,7 @@ public class Matriks {
         return Hasil;
     }
 
-    public Matriks Square() {
+    public Matriks Square() { // extend matriks dengan baris/kolom berisi 0 semua
         if (this.NBrsEff != this.NKolEFF) {
             int N = this.NKolEFF;
             if (this.NBrsEff > this.NKolEFF) {
@@ -544,7 +550,7 @@ public class Matriks {
         return this.Copy();
     }
 
-    public boolean IsEmpty() {
+    public boolean IsEmpty() { // periksa apakah suatu matriks berisi 0 semua
         double epsilon = 1e-10;
         for (int i = 0; i < this.NBrsEff; i++) {
             for (int j = 0; j < this.NKolEFF; j++) {
@@ -556,7 +562,7 @@ public class Matriks {
         return true;
     }
 
-    public boolean AdaBarisKosong() {
+    public boolean AdaBarisKosong() { // periksa apakah salah satu baris berisi 0 semua
         for (int i = 0; i < this.NBrsEff; i++) {
             if (this.AmbilBaris(i).IsEmpty()) {
                 return true;
@@ -565,7 +571,7 @@ public class Matriks {
         return false;
     }
 
-    public boolean AdaKolomKosong() {
+    public boolean AdaKolomKosong() { // periksa apakah salah satu kolom berisi 0 semua
         for (int j = 0; j < this.NKolEFF; j++) {
             if (this.AmbilKolom(j).IsEmpty()) {
                 return true;
@@ -574,7 +580,7 @@ public class Matriks {
         return false;
     }
 
-    public Matriks Copy(){
+    public Matriks Copy(){ // salin matriks
         Matriks Hasil = new Matriks(this.NBrsEff, this.NKolEFF);
         for (int i = 0; i < this.NBrsEff; i++) {
             for (int j = 0; j < this.NKolEFF; j++) {
@@ -587,32 +593,32 @@ public class Matriks {
     public Matriks JadiBasis() {
         Matriks M1 = this.Copy();
         int fill;
-        if (M1.AdaKolomKosong()) {
+        if (M1.AdaKolomKosong()) { // kalau ada kolom kosong, prioritasin ngisi basis kolom tsb
             fill = 0;
-            while ((!this.AmbilKolom(fill).IsEmpty()) && (fill < this.NKolEFF)) {
+            while ((!this.AmbilKolom(fill).IsEmpty()) && (fill < this.NKolEFF)) { // cari kolom yang kosong
                 fill++;
             }
-        } else {
+        } else { // kalau tidak ada kolom kosong, isi dengan basis yang belum muncul
             fill = this.NBrsEff-1;
-            while (this.AdaBasis(fill)) {
+            while (this.AdaBasis(fill)) { // cari sampai basis ke-fill yang belum ada
                 fill--;
             }
         }
         int i = NBrsEff-1;
         Matriks Mtemp;
         Matriks BarisIden = new Matriks(1, this.NKolEFF);
-        for (int j = 0; j < this.NKolEFF; j++) {
+        for (int j = 0; j < this.NKolEFF; j++) { // bikin baris basis seperti [[0,0,0,0,1,0]]
             if (j == fill) {
                 BarisIden.Mat[0][j] = 1;
             } else {
                 BarisIden.Mat[0][j] = 0;
             }
         }
-        while (i >= 0) {
-            Mtemp = M1.TukerBaris(BarisIden, i);
-            if (M1.AmbilBaris(i).IsEmpty()) {
+        while (i >= 0) { // selama i belum mentok
+            Mtemp = M1.TukerBaris(BarisIden, i); // coba tuker baris
+            if (M1.AmbilBaris(i).IsEmpty()) { // kalau baris kosong, langsung isi
                 return Mtemp;
-            } else if ((Mtemp.Determinan() != 0) || (i == 0)) {
+            } else if ((Mtemp.Determinan() != 0) || (i == 0)) { // kalau determinan berubah menjadi tidak 0, atau sudah mentok, langsung isi
                 return Mtemp;
             }
             i--;
@@ -620,19 +626,19 @@ public class Matriks {
         return M1;
     }
 
-    public Matriks ExtSolusi(int NBrs, double fill) {
+    public Matriks ExtSolusi(int NBrs) { // agar dimensi matriks hasil dan matriks persamaan bisa dikalikan
         Matriks b1 = new Matriks(NBrs, 1);
         for (int i = 0; i < NBrs; i++) {
-            if (i < this.NBrsEff) {
+            if (i < this.NBrsEff) { // selama masih terdefine, isi sesuai matriks hasil
                 b1.Mat[i][0] = Mat[i][0];
-            } else {
-                b1.Mat[i][0] = fill;
+            } else { // kalau tidak, isi dengan 0
+                b1.Mat[i][0] = 0.0;
             }
         }
         return b1;
     }
 
-    public Matriks VarMask(Matriks M) { // mengubah konstanta dalam b menjadi 0 dan variabel menjadi k
+    public Matriks VarMask(Matriks M) { // mengubah konstanta dalam b menjadi 0 dan variabel menjadi k untuk mengetahui variabel keberapa
         Matriks b = this.Copy();
         int k = 1;
         for (int i = 0; i < this.NBrsEff; i++) {
@@ -646,7 +652,7 @@ public class Matriks {
         return b;
     }
 
-    public Matriks UnMask(Matriks bvar) {
+    public Matriks UnMask(Matriks bvar) { // mengubah matriks hasil yang tadinya di VarMask dalam 0 dan k menjadi matriks hasil asli dan 0
         Matriks b = this.Copy();
         for (int i = 0; i < this.NBrsEff; i++) {
             if (bvar.Mat[i][0] != 0) {
@@ -656,7 +662,7 @@ public class Matriks {
         return b;
     }
 
-    public Matriks ConjMatriks(Matriks M) {
+    public Matriks ConjMatriks(Matriks M) { // kalau barisnya sama, tidak diubah, kalau beda, jadikan baris berelemen 0 semua
         Matriks M1 = this.Copy();
         boolean beda;
         for (int i = 0; i < this.NBrsEff; i++) {
@@ -674,43 +680,47 @@ public class Matriks {
         return M1;
     }
 
-    public void SPLInverse(Matriks b) {
-        Matriks M1 = this.Square();
-        Matriks b1 = b.ExtSolusi(M1.NBrsEff, 0.0);
-        if (M1.Determinan() != 0) {
+    public String[] SPLInverse(Matriks b) {
+        Matriks M1 = this.Square(); // agar kalkulasi rapih
+        if (M1.Determinan() != 0) { // kalau aman dan determinan tidak 0
             Matriks Mhasil = M1.Inverse().KaliMatriks(b);
-            Mhasil.TulisSPLUnik();
+            String[] output = Mhasil.TulisSPLUnik();
+            return output;
         } else {
+            Matriks b1 = b.ExtSolusi(M1.NBrsEff); // agar bisa mengali A^-1 * b
             boolean valid = true;
-            if (M1.AdaBarisKosong()) {
+            if (M1.AdaBarisKosong()) { // kalau ada baris kosong
                 for (int i = 0; i < M1.NBrsEff; i++) {
                     if (M1.AmbilBaris(i).IsEmpty()) {
-                        valid = valid && (b1.Mat[i][0] == 0);
+                        valid = valid && (b1.Mat[i][0] == 0); // periksa apakah hasil persamaan baris tersebut 0
                     }
                 }
             }
             boolean nonZero = false;
-            for (int j = 0; j < M1.NKolEFF; j++) {
-                if (M1.TukerKolom(b1, j).Determinan() != 0) {
+            for (int j = 0; j < M1.NKolEFF; j++) { // karena determinan 0
+                if (M1.TukerKolom(b1, j).Determinan() != 0) { // periksa apakah ada kolom yang bisa ditukar dengan hasil shg determinan persamaan tidak 0, a.k.a. tidak linear
                     nonZero = true;
                 }
             }
-            if (nonZero || !valid) {
-                System.out.println("SPL tidak memiliki solusi");
+            if (nonZero || !valid) { // jika salah satu terpenuhi
+                String[] output = new String[1];
+                output[0] = "SPL tidak memiliki solusi"; // matriks tidak ada solusi
+                return output;
             } else {
-                int varBebas = 1;
-                Matriks Basis = M1.JadiBasis();
-                while ((Basis.Determinan() == 0) && varBebas <= M1.NBrsEff) {
+                int varBebas = 0; // jika determinan rata 0 (dalam kata lain solusi berbentuk parametrik)
+                Matriks Basis = M1.JadiBasis(); // tuker satu baris dengan baris basis
+                while ((Basis.Determinan() == 0) && varBebas <= M1.NBrsEff) { // hingga determinan != 0
                     Basis = Basis.JadiBasis();
                     varBebas++;
                 }
                 Matriks A = Basis.Inverse();
-                Matriks MTrue = Basis.ConjMatriks(M1);
-                A.TulisSPLParametrik(MTrue, b1);
-                System.out.println("Dengan " + varBebas + " variabel bebas.");
+                Matriks MTrue = Basis.ConjMatriks(M1); // matriks yang jadi basis di-"AND" dengan matriks square
+                String[] output = A.TulisSPLInvParametrik(MTrue, b1, this.NKolEFF);
+                return output;
             }
         }
     }
+
     public boolean IsUnik(){
         Matriks Munik = new Matriks(this.NBrsEff, this.NBrsEff);
         for (int i = 0; i < this.NKolEFF-1; i++) {
@@ -735,29 +745,33 @@ public class Matriks {
         }
         return true;
     }
-    public void TulisSPL() {
+    public String[] TulisSPL() {
+        String[] output = new String[this.NBrsEff];
+        String outputln;
         for (int i = 0; i < this.NBrsEff; i++) {
+            outputln = "";
             for (int j = 0; j < NKolEFF; j++){
                 if (j != NKolEFF -1) {
                     if(Mat[i][j] != 0.0){
                         if (Mat[i][j] < 0) {
-                            System.out.print(" "+Mat[i][j]+"X"+(j+1));
+                            outputln += " "+Mat[i][j]+"X"+(j+1);
                         }
                         else{
                             if(j == 0) {
-                                System.out.print(Mat[i][j] + "X" + (j + 1));
+                                outputln += Mat[i][j] + "X" + (j + 1);
                             }
                             else {
-                                System.out.print(" +" + Mat[i][j] + "X" + (j + 1));
+                                outputln += " + " + Mat[i][j] + "X" + (j + 1);
                             }
                         }
                     }
                 }
                 else {
-                    System.out.println(" = "+Mat[i][j]);
+                    outputln += " = "+Mat[i][j];
                 }
             }
-            System.out.println("");
+            output[i] = outputln;
         }
+        return output;
     }
 }
